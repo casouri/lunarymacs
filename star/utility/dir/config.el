d03cdd82a8c88f0bbb4b05d48bcbcaf71a2d083b
Yuan Fu <casouri@gmail.com>
2018-02-08 14:14:30 -0500

    Refine neotree keymap

--- star/utility/dir/config.el
+++ star/utility/dir/config.el
@@ -2,11 +2,22 @@
 
 (use-package| neotree
   :commands neotree-toggle
-  :init (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  :init
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (setq neo-smart-open t)
+  (setq projectile-switch-project-action 'neotree-projectile-action)
   :config
   (require 'all-the-icons)
   )
 
 (post-config| general
   (default-leader
-    "tn" #'neotree-toggle))
+    "tn" #'neotree-toggle)
+  (general-define-key
+   :states 'normal
+   :keymaps 'neotree-mode-map
+   "TAB" #'neotree-enter
+   "SPC" #'neotree-quick-look
+   "q" #'neotree-hide
+   "RET" #'neotree-enter
+   ))
