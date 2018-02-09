084c7342d5a0b97e53f70d948063f304b8486c2d
Yuan Fu <casouri@gmail.com>
2018-02-09 09:30:06 -0500

    Refine moon/toggle-hidden

--- star/utility/dir/autoload.el
+++ star/utility/dir/autoload.el
@@ -4,5 +4,16 @@
 
 Affect both ranger and neotree."
   (interactive)
-  (setq ranger-show-hidden (not ranger-show-hidden))
-  (setq neo-show-hidden-files (not neo-show-hidden-files)))
+  ;; make sure moon-show-hidden is bounded
+  (unless (boundp 'moon-show-hidden-p)
+    (defvar moon-show-hidden-p
+      ;; set to any exist value, if nor exist, set to nil
+      (if (boundp 'ranger-show-hidden)
+          ranger-show-hidden
+        (if (boundp 'neo-show-hidden-files)
+            neo-show-hidden-files
+          nil))
+      "Whether to show hidden files in file explorers like neotree or ranger."))
+  (setq moon-show-hidden-p (not moon-show-hidden-p))
+  (setq ranger-show-hidden moon-show-hidden-p)
+  (setq neo-show-hidden-files moon-show-hidden-p))
