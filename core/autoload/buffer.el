d9bc643d260e08732cf0aacef1dd4f034ae81c04
Yuan Fu <casouri@gmail.com>
2018-02-15 22:22:10 -0500

    Add rename function

--- core/autoload/buffer.el
+++ core/autoload/buffer.el
@@ -28,3 +28,23 @@ project."
   (interactive)
   "Kill Helper buffer"
   (moon-kill-buffer-and-window "*Help*"))
+
+;;;###autoload
+(defun moon/rename-file ()
+  ;; https://stackoverflow.com/questions/384284/how-do-i-rename-an-open-file-in-emacs
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let* ((name (buffer-name))
+        (filename (buffer-file-name))
+        (basename (file-name-nondirectory filename)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " (file-name-directory filename) basename nil basename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
