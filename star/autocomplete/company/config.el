4dffcdde5493660044314c0c687eabfd2034ac33
Yuan Fu <casouri@gmail.com>
2018-02-15 22:21:45 -0500

    delay load use-package

--- star/autocomplete/company/config.el
+++ star/autocomplete/company/config.el
@@ -1,14 +1,14 @@
 ;;; -*- lexical-binding: t -*-
 
-(use-package| company
+(delay-use-package| company
   :delight (company-mode " Ⓒ")
-  :hook (prog-mode . company-mode)
+  ;; :hook (prog-mode . company-mode)
   :defer t
   :config
-  (setq company-idle-delay 0.2
+  (global-company-mode 1)
+  (setq company-idle-delay 0.1
         company-minimum-prefix-length 2
         company-require-match nil
-        company-dabbrev-ignore-case t
         company-dabbrev-downcase nil)
   (define-key company-active-map (kbd "M-n") nil)
   (define-key company-active-map (kbd "M-p") nil)
@@ -21,9 +21,13 @@
 (add-hook 'org-mode-hook #'short-finger-mode)
 (add-hook 'markdown-mode-hook #'short-finger-mode)
 
-(use-package| yasnippet
+(delay-use-package| yasnippet
   :delight (yas-minor-mode " Ⓨ")
+  ;; :hook (prog-mode . yas-minor-mode)
   :init
   (add-to-list 'load-path (concat moon-emacs-d-dir "snippet/"))
   (add-hook-for-once| prog-mode-hook #'yas-global-mode)
+  :config
+  (yas-minor-mode)
+  (yas-reload-all)
   )
