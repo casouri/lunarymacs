a311b36903fd7a92080ebf1ea9f21a3c179ab549
Yuan Fu <casouri@gmail.com>
2018-02-17 15:46:28 -0500

    Move a lot of setting from basic-ui to core-ui

--- core/core-ui.el
+++ core/core-ui.el
@@ -4,7 +4,7 @@
 ;; Var
 ;;
 
-(defvar moon-homepage-buffer "MOON"
+(defvar moon-homepage-buffer "HOME"
   "The buffer name of the homepage")
 
 (defvar moon-load-theme-hook ()
@@ -40,5 +40,72 @@ And such list cannot be passed into a `font-spec'.")
 
 (defadvice load-theme (after run-load-theme-hook activate)
   (run-hook-with-args 'moon-load-theme-hook))
+
+;;
+;; Color
+;;
+
+(defvar moon-color-book
+  '(doom-blue spacemacs-yellow lunary-white
+              lunary-dark-pink lunary-pink
+              lunary-dark-yellow lunary-yellow
+              spacemacs-gray spacemacs-green
+              spacemacs-light-purple
+              spacemacs-dark-purple
+              powerline-blue poweline-green
+              poweline-yellow mac-red mac-green
+              mac-yellow
+              ))
+
+(defvar doom-blue "#56B0EC"
+  "Blue color of doom-emacs.")
+
+(defvar spacemacs-yellow "DarkGoldenrod2"
+  "Yellow color of spacemacs.")
+
+(defvar spacemacs-light-purple "plum3"
+  "A light purple used in spacemacs.")
+
+(defvar spacemacs-dark-purple "#5D4E79"
+  "Spacemacs purple.")
+
+(defvar spacemacs-gray "#3E3D31"
+  "A dark gray.")
+
+(defvar spacemacs-green "chartreuse3"
+  "A bright green.")
+
+(defvar lunary-white "#DEDDE3"
+  "White color of moon.")
+
+(defvar lunary-dark-yellow "#F3B700"
+  "Dark yellow color of moon.")
+
+(defvar lunary-yellow "#FFD256"
+  "Yellow color of moon.")
+
+(defvar lunary-pink "#E8739F"
+  "Pink(?) color of moon.")
+
+(defvar lunary-dark-pink "#E83077"
+  "Dark pink(?) color of moon.")
+
+(defvar powerline-blue "#289BEC"
+  "Bright blue.")
+
+(defvar powerline-green "#AAC306"
+  "Bright green.")
+
+(defvar powerline-yellow "#DCA809"
+  "Brigh yellow/orange.")
+
+(defvar mac-red "#FA5754"
+  "Red color on mac titlebar.")
+
+(defvar mac-green "#36CF4C"
+  "Green color on mac titlebar.")
+
+(defvar mac-yellow "#FEC041"
+  "Yellow color on mac titlebar.")
 
 (provide 'core-ui)
--- star/basic/basic-ui/config.el
+++ star/basic/basic-ui/config.el
@@ -21,93 +21,6 @@
                                        ")
 
 ;;
-;; Color
-;;
-
-(defvar moon-color-book
-  '(doom-blue spacemacs-yellow lunary-white
-              spacemacs-gray spacemacs-green
-              spacemacs-light-purple
-              spacemacs-dark-purple
-              powerline-blue poweline-green
-              poweline-yellow mac-red mac-green
-              mac-yellow
-              ))
-
-(defvar doom-blue "#56B0EC"
-  "Blue color of doom-emacs.")
-
-(defvar spacemacs-yellow "DarkGoldenrod2"
-  "Yellow color of spacemacs.")
-
-(defvar spacemacs-light-purple "plum3"
-  "A light purple used in spacemacs.")
-
-(defvar spacemacs-dark-purple "#5D4E79"
-  "Spacemacs purple.")
-
-(defvar spacemacs-gray "#3E3D31"
-  "A dark gray.")
-
-(defvar spacemacs-green "chartreuse3"
-  "A bright green.")
-
-(defvar lunary-white "#DEDDE3"
-  "White color of moon.")
-
-(defvar lunary-dark-yellow "#F3B700"
-  "Dark yellow color of moon.")
-
-(defvar lunary-yellow "#FFD256"
-  "Yellow color of moon.")
-
-(defvar lunary-pink "#E8739F"
-  "Pink(?) color of moon.")
-
-(defvar lunary-dark-pink "#E83077"
-  "Dark pink(?) color of moon.")
-
-(defvar powerline-blue "#289BEC"
-  "Bright blue.")
-
-(defvar powerline-green "#AAC306"
-  "Bright green.")
-
-(defvar powerline-yellow "#DCA809"
-  "Brigh yellow/orange.")
-
-(defvar mac-red "#FA5754"
-  "Red color on mac titlebar.")
-
-(defvar mac-green "#36CF4C"
-  "Green color on mac titlebar.")
-
-(defvar mac-yellow "#FEC041"
-  "Yellow color on mac titlebar.")
-
-;;
-;; Cursor Color
-;;
-
-
-(defvar moon-normal-state-cursor-color lunary-white
-  "Cursor color in normal state.")
-
-(defvar moon-insert-state-cursor-color spacemacs-yellow
-  "Cursor color in insert state.")
-
-(defvar moon-maximize-on-startup nil
-  "Whether to maximize screen on startup.")
-
-;;
-;; Config
-;;
-
-;; popup window to right!
-(setq split-height-threshold nil
-      split-width-threshold 80)
-
-;;
 ;; Func
 ;;
 
@@ -138,42 +51,63 @@ MOON is used when buffer's width is less than 86."
   )
 
 ;;
-;; Config
+;; Cursor Color
+;;
+
+
+(defvar moon-normal-state-cursor-color lunary-yellow
+  "Cursor color in normal state.")
+
+(defvar moon-insert-state-cursor-color lunary-pink
+  "Cursor color in insert state.")
+
+(change-cursor-on-hook| evil-normal-state-entry-hook moon-normal-state-cursor-color)
+(change-cursor-on-hook| evil-insert-state-entry-hook moon-insert-state-cursor-color)
+
+
+;;
+;; Maximize
+;;
+
+(defvar moon-maximize-on-startup nil
+  "Whether to maximize screen on startup.")
+
+;; Max screen on startup (or not)
+(when moon-maximize-on-startup
+  (add-to-list 'default-frame-alist '(fullscreen . maximized)))
+
+
+;;
+;; Font
+;;
+
+;; (moon-set-font| :family "Source Code Pro" :weight 'light :size 14)
+(moon-set-font| :family "SF Mono" :weight 'light :size 14)
+
+
+;;
+;; Homepage
 ;;
 
 ;; Homepage
 (add-hook 'moon-init-hook #'moon/draw-homepage t)
 (add-hook 'moon-post-init-hook #'moon-display-benchmark t)
 
+
+;;
+;; Config
+;;
+
+;; Popup window to right!
+(setq split-height-threshold nil
+      split-width-threshold 80)
+
+;; Cleanup
 (menu-bar-mode -1)
 (tooltip-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (blink-cursor-mode -1)
-
-
-;;
-;; font
-;;
-
-
-
-;; (moon-set-font| :family "Source Code Pro" :weight 'light :size 14)
-(moon-set-font| :family "SF Mono" :weight 'light :size 14)
-
-;;
-;; Cursor
-;;
-
-(change-cursor-on-hook| evil-normal-state-entry-hook moon-normal-state-cursor-color)
-(change-cursor-on-hook| evil-insert-state-entry-hook moon-insert-state-cursor-color)
-
-
-;; max screen on startup (or not)
-(when moon-maximize-on-startup
-  (add-to-list 'default-frame-alist '(fullscreen . maximized))
-  )
-
 
 
 ;;
@@ -188,9 +122,3 @@ MOON is used when buffer's width is less than 86."
   :defer t
   :config
   (delight 'eldoc-mode nil "eldoc"))
-
-;; (use-package| shackle
-;;   :defer t
-;;   :init
-;;   (setq shackle-default-rule '(:align 'right :select nil :popup t))
-;;   (make-thread (lambda () (require 'shackle)) "shackle"))
