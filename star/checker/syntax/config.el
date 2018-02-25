4a11c6a46792087ffa54516b2eed62e487da0d65
Yuan Fu <casouri@gmail.com>
2018-02-25 00:32:36 -0500

    Minor update of flycheck loading

--- star/checker/syntax/config.el
+++ star/checker/syntax/config.el
@@ -2,7 +2,9 @@
 
 (use-package| flycheck
   :after exec-path-from-shell
-  :config (flycheck-mode 1)
+  ;; :commands global-flycheck-mode
+  ;; :init (add-hook-for-once| prog-mode-hook (lambda () (global-flycheck-mode 1)))
+  :config (global-flycheck-mode 1)
   )
 
 (post-config| general
