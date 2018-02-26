5b1efd395397eb7f5311d8cdf66df64eecbc3f16
Yuan Fu <casouri@gmail.com>
2018-02-26 01:03:14 -0500

    Fix problem of add-hook-for-once| macro Now it actually workd

--- core/core-package.el
+++ core/core-package.el
@@ -250,18 +250,18 @@ Expressions will be appended."
 
 
 (defmacro add-hook-for-once| (hook func &optional append addlocal removelocal)
-"Add FUNC to HOOK. And remove FUNC from HOOK at first call of FUNC.
+  "Add FUNC to HOOK. And remove FUNC from HOOK at first call of FUNC.
 
 FUNC is a named function
 
 If APPEND or ADDLOCAL is set, APPEND or ADDLOCAL is passed to `add-hook'
 as APPEND and LOCAL. Similarly REMOVELOCAL is passed to `remove-hook' as LOCAL."
-  `(add-hook ',hook
-             (lambda ()
-               (,func)
-               (remove-hook ',hook #',func ,removelocal))
-             ,append
-             ,addlocal))
+  `(fset (symbol-name ,func)
+         (append
+          (symbol-function ,func)
+          '((remove-hook ',hook #',func ,removelocal))))
+  `(add-hook ',hook #',func ,append ,addlocal)
+  )
 
 (defmacro delay-load| (func)
   "Add FUNC to `after-change-major-mode-hook' 
