2d314c9b50de0ddec8511310b9fe420747303e74
Yuan Fu <casouri@gmail.com>
2018-02-28 23:40:03 -0500

    Add docstring

--- core/core.el
+++ core/core.el
@@ -18,7 +18,8 @@
 (defvar moon-init-time nil
   "How long it took for emacs to start")
 
-(defvar lunary-version "0.9.0")
+(defvar lunary-version "0.9.0"
+  "Current version of lunarymacs.")
 
 
 ;; DEBUG
