e99150ac9d58d2c43890d8ee57454fe867a0a506
Yuan Fu <casouri@gmail.com>
2018-03-08 02:31:13 -0500

    Fix binding

--- star/basic/evil/autoload.el
+++ star/basic/evil/autoload.el
@@ -7,16 +7,16 @@
 Dependency:
 - avy"
   (interactive)
-  (define-key evil-normal-state-map "s"   #'avy-goto-char-timer)
-  (define-key evil-insert-state-map "C-a" #'evil-beginning-of-line)
-  (define-key evil-insert-state-map "C-e" #'evil-end-of-line)
+  (define-key evil-normal-state-map "s"         #'avy-goto-char-timer)
+  (define-key evil-insert-state-map (kbd "C-a") #'evil-beginning-of-line)
+  (define-key evil-insert-state-map (kbd "C-e") #'evil-end-of-line)
   )
 
 (defun moon/override-emacs-binding ()
   "Replace some vanilla Emacs bindings."
   (interactive)
-  (define-key evil-normal-state-map "C-u" #'evil-scroll-up)
-  (define-key evil-insert-state-map "C-u" #'evil-shift-right-line)
+  (define-key evil-normal-state-map (kbd "C-u") #'evil-scroll-up)
+  (define-key evil-insert-state-map (kbd "C-u") #'evil-shift-right-line)
   )
 
 
