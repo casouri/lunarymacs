4e7e50cca6c1b46475fdd8cfb8e46523cc446d17
Yuan Fu <casouri@gmail.com>
2018-03-22 20:05:23 -0400

    change loading procedure of flycheck

--- star/checker/syntax/config.el
+++ star/checker/syntax/config.el
@@ -1,10 +1,11 @@
 ;;; -*- lexical-binding: t -*-
 
 (use-package| flycheck
-  :after exec-path-from-shell
   ;; :commands global-flycheck-mode
   ;; :init (add-hook-for-once| prog-mode-hook (lambda () (global-flycheck-mode 1)))
-  :config (global-flycheck-mode 1)
+  :config
+  (require 'exec-path-from-shell)
+  (global-flycheck-mode 1)
   )
 
 (post-config| general
