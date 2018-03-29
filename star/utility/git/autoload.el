f2095bd3353f220d55adbad44c59f5d70c2d670e
Yuan Fu <casouri@gmail.com>
2018-03-29 12:46:22 -0400

    Fix moon/gitlog :fix:
    Now it works properly

--- star/utility/git/autoload.el
+++ star/utility/git/autoload.el
@@ -2,8 +2,9 @@
 
 ;;;###autoload
 (defun moon/gitlog ()
-  "Generate a org file containing current git repo's log."
+  "Generate a temp org-mode buffer containing current git repo's log."
   (interactive)
-  (find-file "./gitlog.org")
-  (insert-buffer-substring (shell-command-to-string
-   "git log --pretty=format:'** %h - %s%n%b' > gitlog.org")))
+  (switch-to-buffer (generate-new-buffer "*gitlog*"))
+  (org-mode)
+  (insert (shell-command-to-string
+   "git log --pretty=format:'** %h - %s%n%b'")))
