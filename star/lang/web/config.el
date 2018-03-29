53a50e094c4741ea308cb5a67ea9b9dd5a4b83a4
Yuan Fu <casouri@gmail.com>
2018-03-29 12:56:33 -0400

    Add web star featuring web-mode :feature:

---
+++ star/lang/web/config.el
@@ -0,0 +1,12 @@
+;;; -*- lexical-binding: t -*-
+
+(use-package| web-mode
+  :mode
+  "\\.phtml\\'"
+  "\\.tpl\\.php\\'"
+  "\\.[agj]sp\\'"
+  "\\.as[cp]x\\'"
+  "\\.erb\\'"
+  "\\.mustache\\'"
+  "\\.djhtml\\'"
+  "\\.html?\\'")
