fe183f7dda25ba2bd75c3e0327135fa842de499c
Yuan Fu <casouri@gmail.com>
2018-04-19 19:04:48 -0400

    C-o behave like o in vim

--- star/basic/edit/config.el
+++ star/basic/edit/config.el
@@ -86,3 +86,15 @@
 
 ;; fold
 ;; (add-hook 'prog-mode-hook (lambda () (outline-minor-mode 1)))
+
+(defun open-next-line (arg)
+      "Move to the next line and then opens a line.
+    See also `newline-and-indent'."
+      (interactive "p")
+      (end-of-line)
+      (open-line arg)
+      (next-line 1)
+      (when newline-and-indent
+        (indent-according-to-mode)))
+
+(global-set-key (kbd "C-o") 'open-next-line)
