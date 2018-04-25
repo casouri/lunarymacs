a6de5eab29883d625b1aa4da84f2efd54e4940c3
Yuan Fu <casouri@gmail.com>
2018-04-25 15:06:15 -0400

    Add org-capture config

--- star/utility/org/config.el
+++ star/utility/org/config.el
@@ -31,3 +31,35 @@
 
 (use-package| ox-hugo
   :after ox)
+
+
+;;
+;; Config
+;;
+
+;;
+;; org-capture
+
+(post-config| general
+  (default-leader
+    "uo" '(:ignore t :which-key "org")
+    "uoc" #'org-capture
+    ))
+
+
+
+(after-load| org-capture
+  (setq org-default-notes-file "~/note/index.org")
+  (setq org-capture-templates
+        (append org-capture-templates
+                `(("l" "Links")
+                  ("la" "Design related links." item (file+olp "~/note/index.org" "Links" "Design links") "- %^L :: ")
+                  ("lp" "programming related links." item (file+olp "~/note/index.org" "Links" "Programming links") "- %^L :: ")
+                  ("lw" "Web related links." item (file+olp "~/note/index.org" "Links" "Web links") "- %^L :: ")
+                  ("lb" "Blog links." item (file+olp "~/note/index.org" "Links" "Blog links") "- %^L :: ")
+                  ("n" "Notes")
+                  ("nr" "Random notes" entry (file+olp "~/note/index.org" "Notes" "Random notes") "*** %?")
+                  ("nv" "Vim notes" entry (file+olp "~/note/index.org" "Notes" "Vim notes") "*** %? ")
+                  ("ne" "Emacs notes" entry (file+olp "~/note/index.org" "Notes" "Emacs notes") "*** %?")
+                  ("nt" "Tutorial notes" entry (file+olp "~/note/index.org" "Notes" "Tutorial notes") "*** %?")
+                  ))))
