3205ebaac1976b5e700ac1a5a93f14f08e545181
Yuan Fu <casouri@gmail.com>
2018-04-25 15:05:53 -0400

    Remove js2-refactor

--- star/lang/javascript/config.el
+++ star/lang/javascript/config.el
@@ -1,13 +1,6 @@
 ;; (use-package| js2-mode
 ;;   :mode "\\.js\\'")
 
-(use-package| js2-refactor
-  :config
-  (js2r-add-keybindings-with-prefix "SPC u r")
-  ;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
-  ;; unbind it.
-  )
-
 (use-package| xref-js2
   :config
   (define-key js-mode-map (kbd "M-.") nil)
--- star/lang/javascript/package.el
+++ star/lang/javascript/package.el
@@ -1,4 +1,3 @@
 (package| js2-mode
-          js2-refactor
           js-format
           xref-js2)
