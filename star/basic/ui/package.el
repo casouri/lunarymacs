efbdf72f6cd6488fc0350411016b7d655d5aaca8
Yuan Fu <casouri@gmail.com>
2018-06-27 09:31:35 +0800

    Remove modern-light-theme

--- star/basic/ui/package.el
+++ star/basic/ui/package.el
@@ -6,5 +6,5 @@
  minions
  all-the-icons
  atom-one-dark-theme
- (modern-light-theme :repo "fuxialexander/modern-light-theme" :fetcher github)
- doom-themes)
+ ;; (modern-light-theme :repo "fuxialexander/modern-light-theme" :fetcher github)
+ (doom-themes :repo "casouri/emacs-doom-themes" :fetcher github))
