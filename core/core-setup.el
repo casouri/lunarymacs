2db101eec2c964f8c19dd4e3ba7af7f6ab69f128
Yuan Fu <casouri@gmail.com>
2018-09-03 19:12:50 -0400

    Align checks

--- core/core-setup.el
+++ core/core-setup.el
@@ -40,7 +40,9 @@ because it's too verbose."
   (dolist (package moon-quelpa-package-list)
     (unless (or (package-installed-p (car package))
                 (require (car package) nil t))
-      (princ (format "Installing %s ... " (symbol-name (car package))))
+      (princ (format "Installing %s %s " (symbol-name (car package))
+                     (make-string (- 30 (length (symbol-name (car package))))
+                                  ?.)))
       (princ (or
               (ignore-errors
                 (silent| (quelpa package))
@@ -51,7 +53,9 @@ because it's too verbose."
   (dolist (package moon-package-list)
     (unless (or (package-installed-p package)
                 (require package nil t))
-      (princ (format "Installing %s ... " (symbol-name package)))
+      (princ (format "Installing %s %s " (symbol-name package)
+                     (make-string (- 30 (length (symbol-name package)))
+                                  ?.)))
       ;; installing packages prints too many messages
       (princ (or
               (ignore-errors
