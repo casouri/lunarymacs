9dd36aa9bf4734bcb48beecc74e95342ceec0850
Yuan Fu <casouri@gmail.com>
2019-09-06 13:10:29 -0400

    Update company config

--- star/company.el
+++ star/company.el
@@ -1,15 +1,28 @@
 ;;; -*- lexical-binding: t -*-
 
+(defvar luna-company-manual t
+  "If t, invoke company manually.")
+
 (load-package company
   :commands (company-mode)
   :config
-  (setq company-idle-delay 0
-        company-minimum-prefix-length 1
+  (if luna-company-manual
+      (setq company-idle-delay 9999)
+    (setq company-idle-delay 0.1))
+  (setq company-minimum-prefix-length 1
         company-dabbrev-downcase nil
         company-tooltip-limit 15)
   (setq-default company-search-filtering t))
 
 (luna-with-eval-after-load 'key.general
+  (when luna-company-manual
+    (general-define-key
+     "<tab>" #'company-indent-or-complete-common)
+    (general-define-key
+     :keymaps '(company-active-map
+                company-search-map)
+     "C-p" #'company-select-previous
+     "C-n" #'company-select-next))
   (general-define-key
    :keymaps '(company-active-map
               company-search-map)
