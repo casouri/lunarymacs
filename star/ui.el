0f8234bae09a7132c822bb254a6fca9553405dad
Yuan Fu <casouri@gmail.com>
2019-09-05 21:55:49 -0400

    Change winner key bindings

--- star/ui.el
+++ star/ui.el
@@ -4,8 +4,8 @@
 
 (luna-with-eval-after-load 'key.general
   (general-define-key
-   "s-<left>" #'winner-undo
-   "s-<right>" #'winner-redo
+   "s-b" #'winner-undo
+   "s-f" #'winner-redo
 
    "s-y" #'luna-toggle-console
    "s-Y" #'luna-toggle-console-window
