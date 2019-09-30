c385aa422ade9a44be99ab1a8b9250fc3b140bd7
Yuan Fu <casouri@gmail.com>
2019-09-29 22:59:21 -0400

    Update checker.el

--- star/checker.el
+++ star/checker.el
@@ -1,16 +1,6 @@
 ;;; -*- lexical-binding: t -*-
 
-;; (load-package flycheck
-;;   ;; :commands global-flycheck-mode
-;;   ;; :init (add-hook-for-once| prog-mode-hook (lambda () (global-flycheck-mode 1)))
-;;   :commands flycheck-mode)
-
 ;;; Key
-
-;; (post-config| general
-;;   (luna-default-leader
-;;     "en" #'hydra-error/flymake-goto-next-error
-;;     "ep" #'hydra-error/flymake-goto-prev-error))
 
 (luna-with-eval-after-load 'key.general
   (luna-default-leader
@@ -20,45 +10,22 @@
     "ec" #'langtool-correct-buffer
     "is" #'synonyms))
 
-;;; Package
-
-;; (load-package flymake-diagnostic-at-point
-;;   :after flymake
-;;   :init (setq flymake-diagnostic-at-point-error-prefix "|")
-;;   :config (add-hook 'flymake-mode-hook #'flymake-diagnostic-at-point-mode))
-
-(add-to-list 'luna-buffer-bottom-list "*Flymake")
-
-;;; Config
-
-;; (post-config| hydra
-;;   (require 'hydra)
-;;   (defhydra hydra-error ()
-;;     "goto-error"
-;;     ("n" flymake-goto-next-error "next")
-;;     ("p" flymake-goto-prev-error "prev")
-;;     ("q" nil "quit")))
-
-;;;; flymake
-
-;; (with-eval-after-load 'emacs-lisp-mode
-;;   (with-eval-after-load 'flymake
-;;     (setq elisp-flymake-byte-compile-load-path
-;;           (append elisp-flymake-byte-compile-load-path load-path))))
-
 ;;; flyspell
-
+;;
+;; install dictionaries: http://wordlist.aspell.net
+;;
 (load-package flyspell
   :hook ((text-mode . flyspell-mode)
          (prog-mode . flyspell-prog-mode))
   :config
-  (with-eval-after-load 'key.general
+  (luna-with-eval-after-load 'key.general
     (general-define-key
      :keymaps 'flyspell-mode-map
      "C-," nil
      "C-M-i" nil
      "C-." nil
      "C-c $" nil))
+  ;; right click on mac touchpad
   (define-key flyspell-mouse-map [down-mouse-3] #'flyspell-correct-word)
   (setq flyspell-issue-message-flag nil)
   ;; (when (executable-find "hunspell")
@@ -67,14 +34,5 @@
   ;;   (setq ispell-dictionary "en_US"))
   )
 
-;; (load-package langtool
-;;   :commands langtool-check
-;;   :config
-;;   (setq langtool-language-tool-server-jar "/usr/local/Cellar/languagetool/4.1/libexec/languagetool-server.jar")
-;;   (setq langtool-java-bin "/usr/bin/java"))
-
 (load-package writegood-mode
   :hook ((fundamental-mode org-mode) . writegood-mode))
-
-;; (load-package (synonyms :fetcher url :url "https://www.emacswiki.org/emacs/download/synonyms.el")
-;;   :commands synonyms)
