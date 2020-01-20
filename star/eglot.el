f777bc6550d670a72d9c57219ee3174a626c6713
Yuan Fu <casouri@gmail.com>
2020-01-20 09:36:44 -0500

    Remove eldoc-box

--- star/eglot.el
+++ star/eglot.el
@@ -19,21 +19,16 @@
   ;; (add-to-list 'eglot-ignored-server-capabilites :hoverProvider)
   )
 
-(load-package eldoc-box
-  :commands (eldoc-box-hover-mode
-             eldoc-box-eglot-help-at-point
-             eldoc-box-helpful-callable
-             eldoc-box-helpful-variable
-             eldoc-box-helpful-key)
-  :init
-  ;; (add-hook 'eglot--managed-mode-hook #'eldoc-box-hover-mode t)
-  (setq eldoc-box-cleanup-interval 0.2
-        eldoc-box-clear-with-C-g t
-        eldoc-box-doc-fold-threshold 200)
-  :config
-  (set-face-attribute 'eldoc-box-body nil :family "SF Pro"))
-
-;;; Function
-
-
-;;; eglot.el ends here
+;; (load-package eldoc-box
+;;   :commands (eldoc-box-hover-mode
+;;              eldoc-box-eglot-help-at-point
+;;              eldoc-box-helpful-callable
+;;              eldoc-box-helpful-variable
+;;              eldoc-box-helpful-key)
+;;   :init
+;;   ;; (add-hook 'eglot--managed-mode-hook #'eldoc-box-hover-mode t)
+;;   (setq eldoc-box-cleanup-interval 0.2
+;;         eldoc-box-clear-with-C-g t
+;;         eldoc-box-doc-fold-threshold 200)
+;;   :config
+;;   (set-face-attribute 'eldoc-box-body nil :family "SF Pro"))
