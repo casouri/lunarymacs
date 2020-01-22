a3da47e129fd733ff1c570b6dfbfccb4e17f43c7
Yuan Fu <casouri@gmail.com>
2020-01-22 16:41:22 -0500

    Remove commented config

--- star/dir.el
+++ star/dir.el
@@ -10,42 +10,6 @@
    "C-c C-s" #'dired-narrow
    "C-c C-o" #'luna-dired-open-file-at-point))
 
-;; (with-eval-after-load 'general
-;;   (luna-default-leader
-;;     "tn" #'neotree-toggle
-;;     "tr" #'ranger
-;;     "th" #'luna-toggle-hidden-file)
-;;   (general-define-key
-;;    :states 'normal
-;;    :keymaps 'neotree-mode-map
-;;    "TAB" #'neotree-enter
-;;    "SPC" #'neotree-quick-look
-;;    "q" #'neotree-hide
-;;    "RET" #'neotree-enter))
-
-;;; Package
-
-;; (load-package neotree
-;;   :commands neotree-toggle
-;;   :init
-;;   (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-;;   (setq neo-smart-open t)
-;;   (setq projectile-switch-project-action 'neotree-projectile-action)
-;;   (setq neo-show-hidden-files t)
-;;   :config
-;;   (require 'all-the-icons))
-
-
-;; (load-package ranger
-;;   :init (setq ranger-show-hidden t
-;;               ;; otherwise ranger binds ranger-key (C-p)
-;;               ;; to `deer-from-dired' in dired mode
-;;               ranger-key nil)
-;;   :commands ranger)
-
-(load-package dired-narrow
-  :commands dired-narrow)
-
 ;;; Config
 
 (add-hook 'dired-mode-hook #'auto-revert-mode)
