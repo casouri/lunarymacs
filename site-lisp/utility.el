151d270806d8b16872fc44917464cf60146a3e84
Yuan Fu <casouri@gmail.com>
2020-02-25 13:39:09 -0500

    Add transform util

--- site-lisp/utility.el
+++ site-lisp/utility.el
@@ -2,6 +2,7 @@
 
 (require 'lunary)
 (require 'luna-f)
+(require 'cl-lib)
 
 ;;; Buffer
 
@@ -294,6 +295,87 @@ and moves point to the previous line."
   (let* ((mode-name (symbol-name major-mode))
          (file-path (expand-file-name mode-name cheatsheet-file-dir)))
     (funcall cheatsheet-display-fn (luna-f-content file-path))))
+
+;;; Transform
+
+(defvar transform-list
+  (mapcar (lambda (x) (mapcar #'char-to-string x))
+          (split-string (string-join
+                         '("*×·⊗⊙ +⊕ |⊦⊨ /÷ \\∖"
+                           "<≤∈∉⊂⊆⊏⊑ >≥∋∌⊃⊇⊐⊒ =≈≠"
+                           "v∨⌄ ^∧⌃ 0∅"
+                           "Rℝ Zℤ Qℚ Nℕ Cℂ"
+                           "aαΑ∀ bβΒ gγΓ dδΔ eεΕ∃ zζΖ hηΗ qθΘ"
+                           "iιΙ kκΚ lλΛ mμΜ nνΝ∩ xξΞ oοΟ pπΠ"
+                           "rρΡ sσΣ tτΤ yυΥ fφΦ cχΧ uψΨ∪ wωΩ")
+                         " ")))
+  "Each car is a starting char, cdr is a list of transformations.")
+
+(defun transform-get-variant-list (char)
+  "Find CHAR in ‘transform-list’, return (index . variant-list).
+Return nil if none found. CHAR should be a string."
+  (catch 'ret
+    (dolist (variant-list transform-list nil)
+      (cl-loop for variant in variant-list
+               for idx from 0 to (1- (length variant-list))
+               if (equal variant char)
+               do (throw 'ret (cons idx variant-list))))))
+
+(defun transform--make-step-fn (variant-list init-idx)
+  "Return a stepping function that steps through each variation.
+At first the index is INIT-IDX.
+VARIANT-LIST is a list of variant characters.
+
+The step function takes a integer “step” that changes the index
+of current variant, e.g. 1 is next, -1 is prev. It returns the
+current index after adding the “step” with current index.
+
+The step function with step across the variant list and change
+the character before point to the current variant."
+  (let ((variant-index (or init-idx 0)))
+    (lambda (step)
+      (setq variant-index (+ step variant-index))
+      (when (eq variant-index (length variant-list))
+        (setq variant-index 0))
+      (when (< variant-index 0)
+        (setq variant-index (1- (length variant-list))))
+      (atomic-change-group
+        (delete-char -1)
+        (insert (nth variant-index variant-list)))
+      (message "%s" (transform-make-message variant-list
+                                            variant-index))
+      variant-index)))
+
+(defun transform-make-message (variant-list index)
+  "Make a string that displays each variant in VARIANT-LIST.
+Highlight the one marked by INDEX."
+  (string-join (cl-loop for variant in variant-list
+                        for idx from 0 to (1- (length variant-list))
+                        if (eq idx index)
+                        collect (propertize variant 'face 'highlight)
+                        else collect variant)
+               " "))
+
+(defun transform-previous-char ()
+  "Transform char before point."
+  (interactive)
+  (when-let ((c (transform-get-variant-list (char-to-string
+                                             (char-before)))))
+    (let* ((index (car c))
+           (variant-list (cdr c))
+           (step-fn (transform--make-step-fn variant-list index))
+           (map (let ((map (make-sparse-keymap)))
+                  (define-key map (kbd "C-n")
+                    (lambda () (interactive) (funcall step-fn 1)))
+                  (define-key map (kbd "C-p")
+                    (lambda () (interactive) (funcall step-fn -1)))
+                  (define-key map (this-command-keys)
+                    (lambda () (interactive) (funcall step-fn 1)))
+                  map)))
+      (funcall step-fn 1)
+      (set-transient-map map t))))
+
+;;; Provide
 
 (provide 'utility)
 
