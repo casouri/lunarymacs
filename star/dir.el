ff3c89b8ead57383e8161199a65646bd3aa48f3e
Yuan Fu <casouri@gmail.com>
2020-02-26 13:47:15 -0500

    Add trivial-copy binding

--- star/dir.el
+++ star/dir.el
@@ -7,7 +7,10 @@
    :keymaps 'dired-mode-map
    "b" #'dired-up-directory
    "q" #'luna-quit-window
-   "C-c C-o" #'luna-dired-open-file-at-point))
+   "C-c C-o" #'luna-dired-open-file-at-point
+   "s-v" #'trivial-copy-paste
+   "s-c" #'trivial-copy-copy
+   "s-M-v" #'trivial-copy-move))
 
 ;;; Config
 
