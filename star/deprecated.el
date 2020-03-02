5c4e62a27877c756b6f79df59b717f7a03842fc4
Yuan Fu <casouri@gmail.com>
2020-03-01 20:06:49 -0500

    Add deprecated configs

---
+++ star/deprecated.el
@@ -0,0 +1,22 @@
+;;;; notmuch
+;; (add-to-list 'load-path "/opt/local/share/emacs/site-lisp/notmuch")
+;; (setq notmuch-init-file (luna-f-join user-emacs-directory "star/notmuch-config.el"))
+;; (setq message-auto-save-directory "~/mail/draft")
+;; (setq message-kill-buffer-on-exit t)
+;; (setq notmuch-search-oldest-first nil)
+;; (require 'notmuch)
+
+;;;; nyan
+;; (nyan-lite-mode)
+;; (setq nyan-wavy-trail t)
+;; enabling this makes highlight on buttons blink
+;; (nyan-start-animation)
+
+;;;; Font
+;; WenYue GuDianMingChaoTi (Non-Commercial Use) W5
+;; WenYue XHGuYaSong (Non-Commercial Use)
+;; WenyueType GutiFangsong (Non-Commercial Use)
+;; SiaoyiWangMingBold
+;; FZQingKeBenYueSongS-R-GB
+;; FZSongKeBenXiuKaiS-R-GB
+
