1858ac85f78f17a49ffb3441f82a69ea1f99e8df
Yuan Fu <casouri@gmail.com>
2020-03-06 22:20:12 -0500

    Use jit-lock

--- site-lisp/sfill.el
+++ site-lisp/sfill.el
@@ -45,12 +45,13 @@
 ;;
 
 (require 'subr-x)
+;; Require solely for ‘buffer-face-mode’, so that we can guess we are
+;; in variable pitch setting or mono space setting. This is necessary
+;; because we can use a much faster function in mono space setting.
+(require 'face-remap)
 
 (defvar-local sfill-column 70
-  "Default fill Column for sfill.")
-
-(defvar-local sfill-variable-pitch-column 70
-  "Column used for variable pitch filling.")
+  "Fill Column for sfill.")
 
 (defvar-local sfill-variable-pitch t
   "Set to non-nil and sfill will assume variable pitch when filling.")
@@ -68,16 +69,16 @@
       (set-face-attribute 'sfill-debug-face nil :inherit 'highlight)
     (set-face-attribute 'sfill-debug-face nil :inherit 'default)))
 
-(defun sfill-insert (string)
-  "Insert STRING at point by overlay."
+(defun sfill-insert-newline ()
+  "Insert newline at point by overlay."
   ;; We shouldn’t need to break line at point-max.
   (if (or (eq (point) (point-max)))
       (error "Cannot insert at the end of visible buffer")
     (let* ((beg (point))
            (end (1+ (point)))
-           (ov (make-overlay beg end)))
+           (ov (make-overlay beg end nil t)))
       (overlay-put ov 'sfill t)
-      (overlay-put ov 'before-string string)
+      (overlay-put ov 'before-string "\n")
       (overlay-put ov 'evaporate t)
       (overlay-put ov 'face 'sfill-debug-face))))
 
@@ -88,85 +89,163 @@
       (when (overlay-get ov 'sfill)
         (delete-overlay ov)))))
 
-(defun sfill-clear-char (char beg end)
-  "Remove CHAR(string) in the region from BEG to END."
+(defun sfill-delete-overlay-at (point)
+  "Delete sfill overlay at POINT."
+  (sfill-clear-overlay point (1+ point)))
+
+(defun sfill-clear-newline (beg end)
+  "Remove newlines in the region from BEG to END."
   (save-excursion
     (goto-char beg)
-    (while (re-search-forward char end t)
+    (while (re-search-forward "\n" end t)
       ;; I can be more intelligent here, but since the break point
       ;; function is from fill.el, better keep in sync with it.
       ;; (see ‘fill-move-to-break-point’)
       (if (and (eq (char-charset (char-before (1- (point)))) 'ascii)
 	       (eq (char-charset (char-after (point))) 'ascii))
           (replace-match " ")
-        (replace-match "")))))
+        (replace-match "")))
+    (put-text-property beg end 'sfill-bol nil)))
 
 (defun sfill-forward-column (column)
-  "Forward COLUMN columns."
-  (while (>= column 0)
-    (forward-char)
-    (setq column (- column (char-width (char-before))))))
+  "Forward COLUMN columns.
 
-(defun sfill-point-at-column-variable-pitch (column bound)
-  "Return point at COLUMN. Works with mono space and variable pitch.
+This only works correctly in mono space setting."
+  (condition-case nil
+      (while (>= column 0)
+        (forward-char)
+        (setq column (- column (char-width (char-before)))))
+    ('end-of-buffer nil)))
 
-BOUND is point where we shouldn’t go beyond. Return nil if the
-desired position is not in window or it is beyond BOUND."
-  ;; X offset from widow’s left edge in pixels. We want to break
-  ;; around this position.
+(defun sfill-move-to-column (column bound)
+  "Go to COLUMN and return (point).
+
+BOUND is point where we shouldn’t go beyond. So if the point at COLUMN
+is beyond BOUND, stop at BOUND. If we go outside the visible portion of
+the window before reaching BOUND, don’t move and return nil."
+  ;; ‘column-x-pos’ is the x offset from widow’s left edge in pixels.
+  ;; We want to break around this position.
   (when-let* ((column-x-pos (* column (window-font-width)))
               (initial-y (cadr (pos-visible-in-window-p nil nil t)))
               (point (posn-point (posn-at-x-y column-x-pos initial-y))))
-    (when (< point bound) point)))
+    (if (eq point nil)
+        nil
+      (goto-char (min point bound)))))
 
-(defun sfill-region-destructive (beg end)
-  "Fill region between BEG and END."
-  (sfill-clear-char "\n" beg end)
-  (sfill-region-1 beg end))
+(defun sfill-go-to-break-point (linebeg bound)
+  "Move to the position where the line should be broken.
+LINEBEG is the beginning of current visual line.
+We don’t go beyond BOUND."
+  (let ((break-point nil)
+        (monop (not buffer-face-mode)))
+    (if monop
+        (sfill-forward-column sfill-column)
+      (while (not break-point)
+        (when (not (setq break-point
+                         (sfill-move-to-column
+                          sfill-column bound)))
+          ;; If we moved out of the visible window,
+          ;; ‘sfill-move-to-column’ returns nil. Recenter and try again.
+          (recenter))))
+    ;; If this (visual) line is the last line of the (visual) paragraph,
+    ;; (point) would be equal to bound, and we want to stay there, so
+    ;; that later we don’t insert newline incorrectly.
+    (unless (>= (point) bound)
+      (fill-move-to-break-point linebeg)
+      (skip-chars-forward " \t"))))
 
-(defun sfill-region (beg end)
-  "Fill region between BEG and END."
-  ;; Save window configuration because we recenter window when text is
-  ;; out of the window.
-  (save-window-excursion
-    (save-excursion
-      ;; (1+ end) to clean the newline overlay at the very end.
-      (sfill-clear-overlay beg (1+ end))
-      (let (linebeg)
-        (goto-char beg)
-        (while (< (point) end)
-          (setq linebeg (point))
-          ;; We cannot use ‘move-to-column’ as fill.el does. Because we
-          ;; break lines with overlays, so if we are at a fake newline,
-          ;; ‘move-to-column’ doesn’t really go forward.
-          (if sfill-variable-pitch
-              (progn
-                (unless (pos-visible-in-window-p)
-                  (recenter 1))
-                (goto-char (or (sfill-point-at-column-variable-pitch
-                                sfill-variable-pitch-column end)
-                               end)))
-            (sfill-forward-column sfill-column))
-          ;; Check again if we are in the desired range.
-          (when (< (point) end)
-            (fill-move-to-break-point linebeg)
-            (skip-chars-forward " \t")
-            (sfill-insert "\n")))))))
+(defsubst sfill-next-break (point bound)
+  "Return the position of the first line break after POINT.
+Don’t go beyond BOUND."
+  (next-single-char-property-change
+   (1+ point)
+   'sfill nil bound))
 
-(defun sfill-jit-lock-fn (beg end)
-  "Fill region between BEG and END.
-Unlike ‘sfill-region’, we don’t remove newlines. Instead, we go
-through each line in the region and wrap them."
+(defsubst sfill-at-break (point)
+  "Return non-nil if POINT is at a line break."
+  (plist-get (text-properties-at point) 'sfill-bol))
+
+(defsubst sfill-prev-break (point bound)
+  "Return the position of the first line break before POINT.
+Don’t go beyond BOUND."
+  (1- (previous-single-char-property-change
+       point 'sfill nil
+       (1+ bound))))
+
+(defun sfill-line (point &optional force)
+  "Fill the line in where POINT is.
+Return (BEG END) where the text is filled. BEG is the visual
+beginning of current live. END is the actual end of line. If
+FORCE is non-nil, update the whole line."
+  (catch 'early-termination
+    (save-window-excursion
+      (save-excursion
+        (if (eq point (point-max))
+            (throw 'early-termination (cons point point)))
+        (let* ((end (line-end-position))
+               (prev-break (if (sfill-at-break point) point
+                             (sfill-prev-break
+                              point (line-beginning-position))))
+               (prev-break (sfill-prev-break
+                            prev-break (line-beginning-position)))
+               next-existing-break
+               (beg prev-break)
+               (match-count 0)
+               (monop (not buffer-face-mode)))
+          (goto-char beg)
+          (while (< (point) end)
+            (setq next-existing-break (sfill-next-break (point) end))
+            (sfill-delete-overlay-at next-existing-break)
+            (sfill-go-to-break-point (point) end)
+            (unless (>= (point) end)
+              (sfill-insert-newline))
+            (if (eq next-existing-break (point))
+                ;; (or (and monop (eq next-existing-break (point)))
+                ;;     (and (not monop)
+                ;;          (< (abs (- next-existing-break (point))) 7)))
+                (setq match-count (1+ match-count)))
+            (if (and (not force) (>= match-count 2))
+                (throw 'early-termination (cons beg end))))
+          (cons beg end))))))
+
+;; Slightly faster but not completely correct
+;;
+;; (defun sfill-line (point &optional force)
+;;   "Fill the line in where POINT is.
+;; Return (BEG END) where the text is filled. BEG is the visual
+;; beginning of current live. END is the actual end of line. If
+;; FORCE is non-nil, update the whole line."
+;;   (catch 'early-termination
+;;     (save-window-excursion
+;;       (save-excursion
+;;         (if (eq point (point-max))
+;;             (throw 'early-termination (cons point point)))
+;;         (let* ((end (line-end-position))
+;;                (prev-break (if (sfill-at-break point) point
+;;                              (sfill-prev-break
+;;                               point (line-beginning-position))))
+;;                next-existing-break
+;;                (beg prev-break))
+;;           (goto-char beg)
+;;           (while (< (point) end)
+;;             (setq next-existing-break (sfill-next-break (point) end))
+;;             (sfill-delete-overlay-at next-existing-break)
+;;             (sfill-go-to-break-point (point) end)
+;;             (unless (>= (point) end)
+;;               (sfill-insert-newline))
+;;             (if (and (not force) (eq next-existing-break (point)))
+;;                 (throw 'early-termination (cons beg end))))
+;;           (cons beg end))))))
+
+(defun sfill-region (&optional beg end force)
+  "Fill each line in the region from BEG to END.
+
+If FORCE is non-nil, update the whole line. BEG and END default
+to beginning and end of the buffer."
   (save-excursion
-    (goto-char beg)
-    (sfill-wrap-line)
-    (while (re-search-forward "\n" end t)
-      (sfill-wrap-line))))
-
-(defun sfill-wrap-line ()
-  "Soft wrap current line with overlay."
-  (interactive)
-  (sfill-region (line-beginning-position) (line-end-position)))
+    (goto-char (or beg (point-min)))
+    (while (re-search-forward "\n" (or end (point-max)) t)
+      (sfill-line (point) force))))
 
 (defun sfill-paragraph ()
   "Fill current paragraph."
@@ -185,14 +264,29 @@ through each line in the region and wrap them."
   "Un-fill region from BEG to END, default to whole buffer."
   (sfill-clear-overlay (or beg (point-min)) (or end (point-max))))
 
+(defun sfill-jit-lock-fn (beg _)
+  "Fill line at where BEG is."
+  (cons 'jit-lock-bounds (sfill-line beg)))
+
+(defun sfill-after-change-fn (beg _ _)
+  "Fill line at where BEG is."
+  (sfill-line beg))
+
+(defvar sfill-mode-map (let ((map (make-sparse-keymap)))
+                         (define-key map (kbd "C-a") #'backward-sentence)
+                         (define-key map (kbd "C-e") #'forward-sentence)
+                         map)
+  "The keymap for minor mode ‘sfill-mode’.")
+
 (define-minor-mode sfill-mode
   "Automatically wrap lines."
   :lighter ""
+  :keymap 'sfill-mode-map
   (if sfill-mode
-      (progn (add-to-list 'jit-lock-functions #'sfill-jit-lock-fn)
-             (sfill-jit-lock-fn (point-min) (point-max)))
-    (setq jit-lock-functions
-          (remove 'sfill-jit-lock-fn jit-lock-functions))
+      (progn
+        (jit-lock-register #'sfill-jit-lock-fn)
+        (jit-lock-refontify))
+    (jit-lock-unregister #'sfill-jit-lock-fn)
     (sfill-unfill)))
 
 (provide 'sfill)
