feb3da73ac6692be97efa855f2ea89c22a2a0276
Yuan Fu <casouri@gmail.com>
2020-03-14 19:49:55 -0400

    Update elisp config

--- star/elisp.el
+++ star/elisp.el
@@ -13,22 +13,9 @@
    "wf" #'eval-defun
    "wb" #'eval-buffer))
 
-
-;;; Config
-
-(dolist (hook '(emacs-lisp-mode-hook lisp-interaction-mode-hook))
-  (add-hook hook #'flymake-mode)
-  (add-hook hook #'company-mode)
-  (add-hook hook #'aggressive-indent-mode))
-
-
 ;;; Package
 
 ;; interfere with Emacs development
 ;; (load-package sly-el-indent
 ;;   :commands sly-el-indent-setup
 ;;   :hook (emacs-lisp-mode-hook . sly-el-indent-setup))
-
-;; (load-package nameless
-;;   :config (setq nameless-prefix "::")
-;;   :hook (emacs-lisp-mode-hook . nameless-mode))
