646bd1de3923274e31ef6b589093c38c001d8200
Yuan Fu <casouri@gmail.com>
2020-03-22 21:07:07 -0400

    Update manual hack

--- star/hacks.el
+++ star/hacks.el
@@ -2,31 +2,31 @@
 
 ;;; Helpful
 
-(defvar helpful-html-manual-dir "/Users/yuan/emacs/doc/lispref/elisp.html"
-  "Absolute path to the directory where HTML manuals are.")
+(defvar helpful-html-manual-base-url "https://archive.casouri.cat/emacs-manuals/master/elisp"
+  "Base URL for the online manual. No trailing slash.")
+
+(defvar helpful-html-manual-index (expand-file-name "site-lisp/index.html" user-emacs-directory))
 
 (define-minor-mode helpful-html-manual-mode
   "View HTML manuals in helpful."
   :lighter ""
   :global t
   (if helpful-html-manual-mode
-      (advice-add #'helpful--manual :override #'helpful--html-manual)
+      (advice-add #'helpful--manual :around #'helpful--html-manual)
     (advice-remove #'helpful--manual #'helpful--html-manual)))
 
-(defun helpful--html-manual (button)
+(defun helpful--html-manual (oldfn button)
   "Open the HTML manual for the symbol that this BUTTON represents."
   (let ((sym (symbol-name (button-get button 'symbol)))
-        (index-file (expand-file-name "Index.html" helpful-html-manual-dir))
-        manual-page)
+        (index-file helpful-html-manual-index))
     (with-temp-buffer
       (insert-file-contents index-file)
       (goto-char (point-min))
       (if (not (re-search-forward
                 (format "<a href=\"\\(.*\\)\"><code>%s</code>" sym) nil t))
-          (message "No manual index for %s" sym)
-        (message "%s" (setq manual-page (expand-file-name
-                                         (match-string 1) helpful-html-manual-dir)))
-        (shell-command-to-string (format "open 'file://%s'" manual-page))))))
+          (funcall oldfn button)
+        (browse-url
+         (format "%s/%s" helpful-html-manual-base-url (match-string 1)))))))
 
 (with-eval-after-load 'helpful
   (helpful-html-manual-mode))
