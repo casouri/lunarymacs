5b5cb11b059b3c6745f7f67a7b469253864813dc
Yuan Fu <casouri@gmail.com>
2020-04-20 16:38:08 -0400

    Update mode-line

--- star/mode-line.el
+++ star/mode-line.el
@@ -28,12 +28,13 @@ else return STR."
       empty-return
     str))
 
-(defvar luna-flymake-mode-line-map (let ((map (make-sparse-keymap)))
-                                     (define-key map (vector 'mode-line
-                                                             mouse-wheel-up-event) #'flymake-goto-prev-error)
-                                     (define-key map (vector 'mode-line
-                                                             mouse-wheel-down-event) #'flymake-goto-next-error)
-                                     map))
+(defvar luna-flymake-mode-line-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map
+      (vector 'mode-line mouse-wheel-up-event) #'flymake-goto-prev-error)
+    (define-key map
+      (vector 'mode-line mouse-wheel-down-event) #'flymake-goto-next-error)
+    map))
 
 (defun luna-dedicated-window-mode-line ()
   (if (window-dedicated-p)
@@ -57,42 +58,46 @@ else return STR."
                      (flymake--backend-state-diags state)))
              flymake--backend-state)
     (apply #'concat
-           (mapcar (lambda (args)
-                     (apply (lambda (num str face pad)
-                              (concat (propertize
-                                       (format str num)
-                                       'face face
-                                       'keymap luna-flymake-mode-line-map
-                                       'help-echo (format "%d running backens\nScroll up/down: previous/next diagnose"
-                                                          (length running)))
-                                      (propertize pad 'face '(:foreground "gray"))))
-                            args))
-                   `((,(length (gethash :error diags-by-type)) "%d " error "|")
-                     (,(length (gethash :warning diags-by-type)) " %d " warning "|")
-                     (,(length (gethash :note diags-by-type)) " %d" success ""))))))
+           (mapcar
+            (lambda (args)
+              (apply
+               (lambda (num str face pad)
+                 (concat
+                  (propertize
+                   (format str num)
+                   'face face
+                   'keymap luna-flymake-mode-line-map
+                   'help-echo (format "%d running backens\nScroll up/down: previous/next diagnose"
+                                      (length running)))
+                  (propertize pad 'face '(:foreground "gray"))))
+               args))
+            `((,(length (gethash :error diags-by-type)) "%d " error "|")
+              (,(length (gethash :warning diags-by-type)) " %d " warning "|")
+              (,(length (gethash :note diags-by-type)) " %d" success ""))))))
 
-(defvar luna-many-space "    ")
-
-(setq-default mode-line-format '((:eval (luna-root-lighter))
-                                 (:eval (luna-dedicated-window-mode-line))
-                                 ;; (:eval (luna-edit-lighter))
-                                 vc-mode
-                                 luna-many-space
-                                 ;; (:eval (if (bound-and-true-p eyebrowse-mode) (eyebrowse-mode-line-indicator) ""))
-                                 "%b"
-                                 luna-many-space
-                                 mode-line-modes
-                                 luna-many-space
-                                 (:eval (if (bound-and-true-p flymake-mode) (luna-flymake-mode-line) "OK"))
-                                 luna-many-space
-                                 ;; misc info beg
-                                 mode-line-misc-info
-                                 ;; misc info end
-                                 luna-many-space
-                                 "%I"
-                                 luna-many-space
-                                 (:eval (if (bound-and-true-p nyan-lite-mode) (nyan-lite-mode-line) "ฅ Φ ω Φ ฅ"))
-                                 luna-many-space
-                                 "%p"
-                                 ;; "  %l:%c"
-                                 mode-line-end-spaces))
+(setq-default mode-line-format
+              (let ((spaces "   "))
+                `((:eval (luna-root-lighter))
+                  (:eval (luna-dedicated-window-mode-line))
+                  ;; (:eval (luna-edit-lighter))
+                  vc-mode
+                  ,spaces
+                  ;; (:eval (if (bound-and-true-p eyebrowse-mode)
+                  ;;            (eyebrowse-mode-line-indicator) ""))
+                  "%b"
+                  ,spaces
+                  mode-line-modes
+                  ,spaces
+                  (:eval (if (bound-and-true-p flymake-mode)
+                             (luna-flymake-mode-line) "OK"))
+                  ,spaces
+                  mode-line-misc-info
+                  ,spaces
+                  "%I"
+                  ,spaces
+                  (:eval (if (bound-and-true-p nyan-lite-mode)
+                             (nyan-lite-mode-line) "ฅ Φ ω Φ ฅ"))
+                  ,spaces
+                  "%p"
+                  ;; "  %l:%c"
+                  mode-line-end-spaces)))
