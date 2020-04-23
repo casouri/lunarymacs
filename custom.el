2f89459cfbaa6ec11a062bb04a0f63a793f4e116
Yuan Fu <casouri@gmail.com>
2020-04-23 17:50:11 -0400

    Move configs to customize

---
+++ custom.el
@@ -0,0 +1,81 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ad-redefinition-action 'accept)
+ '(ansi-color-names-vector
+   ["#f0f0f0" "#e45649" "#50a14f" "#986801" "#4078f2" "#a626a4" "#0184bc" "#1b2229"])
+ '(apropos-do-all t)
+ '(auto-save-timeout 5)
+ '(compilation-always-kill t)
+ '(compilation-ask-about-save nil)
+ '(confirm-nonexistent-file-or-buffer t)
+ '(create-lockfiles nil)
+ '(custom-safe-themes
+   '("2eb66b8818a410a3f7ff8ea03efb8418aef485c7f58d6ece02be12b329a4e13a"))
+ '(debug-on-error nil)
+ '(delete-auto-save-files nil)
+ '(delete-by-moving-to-trash t)
+ '(display-line-numbers-width 3)
+ '(ediff-split-window-function 'split-window-horizontally)
+ '(electric-pair-mode t)
+ '(electric-quote-mode t)
+ '(enable-recursive-minibuffers t)
+ '(flywrap-debug-mode t)
+ '(help-window-select t)
+ '(history-length 500)
+ '(idle-update-delay 0.5)
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
+ '(kill-ring-max 100)
+ '(minibuffer-electric-default-mode t)
+ '(mouse-scroll-delay 0)
+ '(mouse-wheel-flip-direction t)
+ '(mouse-wheel-progressive-speed nil)
+ '(mouse-wheel-scroll-amount '(2))
+ '(mouse-wheel-tilt-scroll t)
+ '(ns-pop-up-frames nil nil nil "This way emacsclient doesn't open new frames.")
+ '(package-selected-packages
+   '(git-timemachine realgud-lldb debbugs magit benchmark-init yasnippet expand-region xr geiser magit-todos magit-patch-changelog proof-general "eglot" ivy-xref srefactor helpful imenu-list eglot racket-mode gnu-apl-mode ivy-prescient counsel swiper yaoddomuse matlab-mode ivy cdlatex tide realgud ccls flycheck hierarchy snail ztree lua-mode sly web-mode quickrun mips-mode yaml-mode markdown-mode company flyspell hl-todo f haskell-mode ws-butler writegood-mode which-key visual-regexp use-package sage-shell-mode rainbow-mode rainbow-delimiters pyvenv org-download olivetti minions latex-preview-pane htmlize highlight-parentheses general eyebrowse diff-hl buffer-move auctex aggressive-indent))
+ '(recentf-max-saved-items 300)
+ '(ring-bell-function 'ignore)
+ '(savehist-additional-variables '(tablist-named-filter extended-command-history))
+ '(savehist-mode t)
+ '(scroll-conservatively 101)
+ '(sentence-end-double-space nil)
+ '(use-dialog-box nil)
+ '(vc-annotate-background "#fafafa")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#50a14f")
+    (cons 40 "#688e35")
+    (cons 60 "#807b1b")
+    (cons 80 "#986801")
+    (cons 100 "#ae7118")
+    (cons 120 "#c37b30")
+    (cons 140 "#da8548")
+    (cons 160 "#c86566")
+    (cons 180 "#b74585")
+    (cons 200 "#a626a4")
+    (cons 220 "#ba3685")
+    (cons 240 "#cf4667")
+    (cons 260 "#e45649")
+    (cons 280 "#d2685f")
+    (cons 300 "#c07b76")
+    (cons 320 "#ae8d8d")
+    (cons 340 "#383a42")
+    (cons 360 "#383a42")))
+ '(vc-annotate-very-old-color nil)
+ '(visible-cursor nil)
+ '(winner-mode t)
+ '(xref-prompt-for-identifier
+   '(not xref-find-references xref-find-definitions xref-find-definitions-other-window xref-find-definitions-other-frame)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "SF Mono" :foundry "nil" :slant normal :weight normal :height 130 :width normal))))
+ '(ghelp-entry ((t :inherit nil))))
--- star/builtin-config.el
+++ star/builtin-config.el
@@ -5,74 +5,26 @@
 (fset #'yes-or-no-p #'y-or-n-p) ; y/n instead of yes/no
 
 (setq-default
- ad-redefinition-action              'accept     ; silence advised function warnings
- apropos-do-all                      t           ; make `apropos' more useful
- compilation-always-kill             t           ; kill compilation process before starting another
- compilation-ask-about-save          nil         ; save all buffers on `compile'
- confirm-nonexistent-file-or-buffer  t
- idle-update-delay                   2           ; update ui less often
+ backup-directory-alist `((".*" . ,(luna-f-join luna-cache-dir "backup")))
+ auto-save-list-file-prefix (luna-f-join luna-cache-dir "auto-save-list/saves-")
 
- ;; auto-save
- create-lockfiles                    nil
- history-length                      500
- make-backup-files                   t
- auto-save-default                   t
- delete-auto-save-files              nil
- backup-directory-alist              `((".*" . ,(luna-f-join luna-cache-dir "backup")))
- auto-save-list-file-prefix          (luna-f-join luna-cache-dir "auto-save-list/saves-")
- auto-save-timeout                   5
+ abbrev-file-name (expand-file-name "abbrev.el" luna-cache-dir)
+ recentf-save-file (expand-file-name "recentf" luna-cache-dir)
+ tramp-persistency-file-name (expand-file-name "tramp" luna-cache-dir)
+ bookmark-default-file (expand-file-name "bookmarks" luna-cache-dir)
+ savehist-file (expand-file-name "history" luna-cache-dir)
+ auto-save-file-name-transforms (list (list ".*" (luna-f-join luna-cache-dir "auto-save/") t)
+                                      (list "\\`/[^/]*:\\([^/]*/\\)*\\([^/]*\\)\\'"
+                                            (luna-f-join luna-cache-dir "auto-save-remote") t))
 
- ;; files
- abbrev-file-name                    (expand-file-name "abbrev.el" luna-cache-dir)
- recentf-save-file                   (expand-file-name "recentf" luna-cache-dir)
- recentf-max-saved-items             300
- tramp-persistency-file-name         (expand-file-name "tramp" luna-cache-dir)
- bookmark-default-file               (expand-file-name "bookmarks" luna-cache-dir)
- delete-by-moving-to-trash           t
- savehist-file                       (expand-file-name "history" luna-cache-dir)
- auto-save-file-name-transforms      (list (list ".*" (luna-f-join luna-cache-dir "auto-save/") t)
-                                           (list "\\`/[^/]*:\\([^/]*/\\)*\\([^/]*\\)\\'"
-                                                 (luna-f-join luna-cache-dir "auto-save-remote") t))
-
- ;; edit
- indent-tabs-mode                    nil
- sentence-end-double-space           nil
- kill-ring-max                       100
-
- ;; ui
- use-dialog-box                      nil
- visible-cursor                      nil
- use-dialog-box                      nil
- visible-bell                        nil
- frame-title-format                  '("%f")               ; current file name
- display-line-numbers-width          3
- ns-pop-up-frames                    nil                   ; no new frame when emacsclient connected
- )
-
-;;;; start up screen
-(setq inhibit-startup-screen t)
+ ;; current file name
+ frame-title-format '("%f"))
 
 ;;;; minibuffer
-(setq enable-recursive-minibuffers t
-      ;; keep the point out of the minibuffer
-      minibuffer-prompt-properties
-      '(read-only t point-entered minibuffer-avoid-prompt
-                  face minibuffer-prompt))
-
-;;;; modes
-(blink-cursor-mode -1)
-(electric-pair-mode)
-(electric-quote-mode)
-(minibuffer-electric-default-mode)
-
-;;;; smooth scrolling
-(setq scroll-conservatively 101
-      mouse-wheel-scroll-amount '(2)
-      mouse-wheel-flip-direction t
-      mouse-wheel-tilt-scroll t)
-;; diabled for emacs-mac port
-;; (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
-(setq mouse-wheel-follow-mouse t) ;; scroll window under mouse
+;; keep the point out of the minibuffer
+;; (setq minibuffer-prompt-properties
+;;       '(read-only t point-entered minibuffer-avoid-prompt
+;;                   face minibuffer-prompt))
 
 ;;;; utf-8
 (prefer-coding-system 'utf-8)
@@ -81,16 +33,6 @@
 (set-keyboard-coding-system 'utf-8)
 (setq-default buffer-file-coding-system 'utf-8)
 (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
-
-;;;; split screen vertically in ediff
-(setq ediff-split-window-function #'split-window-horizontally)
-
-;;;; help
-(setq help-window-select t)
-
-;;;; Xref
-(setq xref-prompt-for-identifier
-      '(not xref-find-references xref-find-definitions xref-find-definitions-other-window xref-find-definitions-other-frame))
 
 ;;;; Term mouse
 (unless window-system
@@ -106,13 +48,6 @@
 
 ;;;; "Dangerous Commands"
 (put 'narrow-to-page 'disabled nil)
-
-;;;; Save history
-(savehist-mode)
-(add-to-list 'savehist-additional-variables 'extended-command-history)
-
-;;;; winner
-(winner-mode)
 
 ;;;; hideshow
 (add-hook 'prog-mode-hook #'hs-minor-mode)
