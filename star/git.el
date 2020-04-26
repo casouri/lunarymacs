f84471b8d56e23f61ab047b35596797a9f47df41
Yuan Fu <casouri@gmail.com>
2020-04-26 12:54:57 -0400

    Add magit command

--- star/git.el
+++ star/git.el
@@ -12,10 +12,22 @@
   :config
   (define-key magit-mode-map (kbd "<tab>") 'magit-section-toggle)
   (add-to-list 'luna-buffer-bottom-list "magit:")
-  (add-to-list 'luna-buffer-bottom-list "magit-process:"))
+  (add-to-list 'luna-buffer-bottom-list "magit-process:")
+  (define-transient-command magit-patch-apply-buffer (buffer &rest args)
+    "Apply the patch buffer BUFFER."
+    (interactive
+     (list (read-buffer "Apply patch in buffer: " nil t)
+           (transient-args 'magit-patch-apply)))
+    (with-temp-buffer
+      (insert-buffer-substring-no-properties buffer)
+      (magit-run-git-with-input "apply" args "-")
+      (magit-refresh)))
+  (transient-append-suffix 'magit-patch-apply "a"
+    '("b"  "Apply patch in buffer" magit-patch-apply-buffer)))
 
 (load-package magit-patch-changelog
   :after magit)
 
 (load-package magit-todos
   :hook (magit-mode . magit-todos-mode))
+
