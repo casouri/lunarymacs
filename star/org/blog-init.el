be958169ffa1865099bf26f1442809331b8a6fd2
Yuan Fu <casouri@gmail.com>
2020-06-26 14:31:05 -0400

    Use new theme for blog publish

--- star/org/blog-init.el
+++ star/org/blog-init.el
@@ -3,14 +3,17 @@
 (add-to-list 'load-path
              (expand-file-name "site-lisp"
                                user-emacs-directory))
+(add-to-list 'custom-theme-load-path
+             (expand-file-name "site-lisp"
+                               user-emacs-directory))
 (require 'luna-f)
 (require 'lunary)
 (require 'cowboy)
 (setq package-user-dir (expand-file-name "package" user-emacs-directory))
 (package-initialize)
 (cowboy-add-load-path)
-(require 'doom-themes)
-(load-theme 'doom-one-light t)
+(require 'light-theme)
+(load-theme 'light t)
 (luna-load-relative "star/org/blogs.el")
 (require 'htmlize)
 (setq auto-save-default nil
