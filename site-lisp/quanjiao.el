e0ad48f3ee888fd5b6b59c0cf4089767056fb908
Yuan Fu <casouri@gmail.com>
2020-07-18 16:11:40 -0400

    Update quanjiao

--- site-lisp/quanjiao.el
+++ site-lisp/quanjiao.el
@@ -14,19 +14,6 @@
   "Face for full width quotation marks."
   :group 'convenience)
 
-;; (defun quanjiao-fontify (beg end)
-;;   "Fontify functions for jit-lock."
-;;   (save-excursion
-;;     (goto-char beg)
-;;     (while (re-search-forward "[‘’“”]" end t)
-;;       (when (or (memq (aref char-script-table (char-after))
-;;                       '(han cjk-misc))
-;;                 (memq (aref char-script-table (char-before (1- (point))))
-;;                       '(han cjk-misc)))
-;;         (put-text-property (1- (point)) (point) 'font-lock-face 'quanjiao-quote)
-;;         (put-text-property (1- (point)) (point) 'face 'quanjiao-quote)))
-;;     (cons 'jit-lock-bounds (cons beg end))))
-
 (defun quanjiao-matcher (limit)
   "Matcher for font-lock."
   (and (re-search-forward "[‘’“”]" limit t)
