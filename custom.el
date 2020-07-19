ba5b51520066ae3ada05c498e6bb71d46e5e0358
Yuan Fu <casouri@gmail.com>
2020-07-19 17:27:08 -0400

    Add new customs

--- custom.el
+++ custom.el
@@ -46,7 +46,7 @@
    '(("melpa" . "http://melpa.org/packages/")
      ("gnu" . "https://elpa.gnu.org/packages/")))
  '(package-selected-packages
-   '(smex latex-preview-pane auctex company-math restclient php-mode all-the-icons-dired yasnippet yaml-mode xr wucuo ws-butler writegood-mode which-key web-mode visual-regexp use-package tuareg tree-sitter-lang-c tide srefactor sly sage-shell-mode rainbow-mode rainbow-delimiters racket-mode quickrun pyvenv proof-general org-download orderless olivetti ob-mermaid mips-mode minions memory-usage matlab-mode magit-todos magit-patch-changelog lua-mode ivy-xref imenu-list icomplete-vertical htmlize highlight-parentheses hierarchy helpful haskell-mode gnu-apl-mode git-timemachine general geiser eyebrowse eglot edit-indirect dumb-jump diff-hl debbugs counsel company-posframe cdlatex ccls buffer-move benchmark-init auto-pause aggressive-indent))
+   '(ccls company valign separedit ghelp yasnippet wucuo tree-sitter tree-sitter-lang-c transient tide sly projectile markdown-mode lsp-mode geiser eglot debbugs magit smex latex-preview-pane auctex restclient php-mode yaml-mode xr ws-butler writegood-mode which-key web-mode visual-regexp tuareg srefactor sage-shell-mode rainbow-mode rainbow-delimiters racket-mode quickrun pyvenv proof-general org-download olivetti mips-mode minions memory-usage matlab-mode magit-todos magit-patch-changelog lua-mode ivy-xref htmlize highlight-parentheses hierarchy helpful haskell-mode gnu-apl-mode git-timemachine general eyebrowse edit-indirect diff-hl counsel cdlatex buffer-move benchmark-init auto-pause aggressive-indent))
  '(recentf-max-saved-items 300)
  '(ring-bell-function 'ignore)
  '(safe-local-variable-values '((git-commit-major-mode . git-commit-elisp-text-mode)))
@@ -57,10 +57,13 @@
  '(scroll-margin 4)
  '(send-mail-function 'mailclient-send-it)
  '(sentence-end-double-space nil)
+ '(split-height-threshold nil)
  '(split-width-threshold 90)
  '(tool-bar-mode t)
  '(use-dialog-box nil)
  '(use-package-hook-name-suffix "")
+ '(user-full-name "Yuan Fu")
+ '(user-mail-address "casouri@gmail.com")
  '(vc-annotate-background "#fafafa")
  '(vc-annotate-color-map
    (list
