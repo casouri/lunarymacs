d6630263d589d42f9c1abd6854c5adfb4b15eed6
Yuan Fu <casouri@gmail.com>
2020-07-20 21:21:33 -0400

    Update recipe

--- star/recipe.el
+++ star/recipe.el
@@ -4,7 +4,8 @@
         (matlab . (:http "https://git.code.sf.net/p/matlab-emacs/src"))
         (nerd-font . (:repo "twlz0ne/nerd-fonts.el"))
         (julia-emacs . (:repo "JuliaEditorSupport/julia-emacs"))
-        (separedit . (:repo "twlz0ne/separedit.el" :dependency (edit-indirect dash)))
+        (separedit . (:repo "twlz0ne/separedit.el"
+                            :dependency (edit-indirect dash)))
         (package-demo . (:repo "vermiculus/package-demo"))
         (ghelp . (:repo "casouri/ghelp" :option (:full-clone t)))
         (sly-el-indent . (:repo "cireu/sly-el-indent"))
@@ -13,4 +14,5 @@
         (wgrep . (:repo "mhayashi1120/Emacs-wgrep"))
         (ghelp . (:repo "casouri/ghelp" :option (:full-clone t)))
         (valign . (:repo "casouri/valign" :option (:full-clone t)))
-        (expand-region . (:repo "casouri/expand-region.el" :option (:full-clone t)))))
+        (expand-region . (:repo "casouri/expand-region.el"
+                                :option (:full-clone t)))))
