7bcca04b259e1a45b475e31cc68d2874bba024d7
Yuan Fu <casouri@gmail.com>
2020-07-20 21:02:14 -0400

    Update cowboy

--- site-lisp/cowboy.el
+++ site-lisp/cowboy.el
@@ -80,11 +80,8 @@ FORMAT and BODY is the same as in ‘with-demoted-errors’."
   "Return t if PACKAGE (symbol) is installed, nil if not."
   (if (cowgirl-use-cowboy package)
       (cowboy-installed-p package)
-    (and (or (package-installed-p package)
-             (condition-case nil
-                 (find-library-name (symbol-name package))
-               (error nil)))
-         t)))
+    (locate-file (symbol-name package) load-path
+                 '(".el" ".el.gz" ".so" ".so.gz"))))
 
 (defun cowboy-installed-p (package)
   "Return t if PACKAGE (symbol) is installed, nil if not."
