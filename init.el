c0488e129f4c417bf16746f0bc0e845b3cc4dcc8
Yuan Fu <casouri@gmail.com>
2020-07-25 14:36:13 -0400

    Update server config

--- init.el
+++ init.el
@@ -103,7 +103,7 @@
 ;;;; server
 ;; checking whether server started can be slow
 ;; see emacs-horror
-(run-with-idle-timer 3 nil (lambda () (server-start t t)))
+(run-with-idle-timer 3 nil (lambda () (ignore-errors (server-start t t))))
 
 ;;;; vterm
 ;; (luna-load-relative "star/term.el")
