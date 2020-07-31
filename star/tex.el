870c054493a0f58131af85a903a8f3d82ba85a7a
Yuan Fu <casouri@gmail.com>
2020-07-31 18:51:45 -0400

    Update tex

--- star/tex.el
+++ star/tex.el
@@ -1,15 +1,22 @@
 ;; lexical-binding: t; -*-
 
+;;; Keys
+
+(luna-def-key
+ :TeX-mode-map
+ "$" nil)
+
 ;;; Packages
 
 (add-to-list 'luna-package-list 'auctex)
 
-(load-package cdlatex
-  :hook (LaTeX-mode-hook . cdlatex-mode))
+;; (load-package cdlatex
+;;   :hook (LaTeX-mode-hook . cdlatex-mode))
 
 (add-to-list 'luna-package-list 'eglot)
 (with-eval-after-load 'eglot
   (add-to-list 'eglot-server-programs '(latex-mode . ("digestif"))))
+
 ;;; Config
 
 (defun tex-mode-setup ()
@@ -28,45 +35,3 @@
 (with-eval-after-load 'tex
   (add-to-list 'TeX-command-list
                '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t)))
-
-;; (with-eval-after-load 'luna-general-config
-;;   (luna-default-leader
-;;     :keymaps 'tex-mode-map
-;;     "tp" #'latex-preview-pane-mode))
-
-;; (load-package latex-preview-pane
-;;   :commands (latex-preview-pane-mode))
-
-;; (load-package company-math
-;;   :commands (luna-latex-company-setup)
-;;   :init
-;;   (add-hook
-;;    'LaTeX-mode-hook
-;;    (lambda ()
-;;      (require 'company-math)
-;;      (setq-local company-backends
-;;                  (append '((company-math-symbols-latex
-;;                             company-latex-commands))
-;;                          company-backends)))))
-
-;; (load-package webkit-katex-render
-;;   :commands webkit-katex-render-mode)
-
-;; (load-package auctex
-;;   :mode ("\\.tex\\'" . latex-mode)
-;;   :init
-;;   (setq TeX-parse-self t)
-;;   (setq-default TeX-master nil)
-;;   (add-hook 'latex-mode-hook #'luna-require-auctex)
-;;   ;; (setq TeX-electric-math (cons "$" "$")) ; too slow
-;;   ;; (setq TeX-command-default "XeLaTeX")
-;;   ;; (setq TeX-show-compilation t)
-;;   (setq TeX-save-query nil)
-;;   (add-hook 'LaTeX-mode-hook
-;;             (lambda ()
-;;               (add-to-list 'TeX-command-list
-;;                            '("XeLaTeX" "%`xelatex%(mode)%' %t"
-;;                              TeX-run-TeX nil t))
-;;               ;; tex-insert-dollar is useless and slow
-;;               (define-key TeX-mode-map (kbd "$") nil))))
-
