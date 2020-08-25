43a07718295405c60d4cc3c254b0437702d87ed6
Yuan Fu <casouri@gmail.com>
2020-08-25 11:33:02 -0400

    Add screenshot

--- site-lisp/utility.el
+++ site-lisp/utility.el
@@ -360,6 +360,19 @@ E.g. SURNAME (c) to symbol ©."
               ;; Don’t separate CJK characters.
               (t (replace-match "")))))))
 
+;;; Screenshot
+
+(defun screenshot-svg ()
+  "Save a screenshot of the current frame as an SVG image.
+Saves to a temp file and puts the filename in the kill ring."
+  (interactive)
+  (let* ((filename (make-temp-file "Emacs" nil ".svg"))
+         (data (x-export-frames nil 'svg)))
+    (with-temp-file filename
+      (insert data))
+    (kill-new filename)
+    (message filename)))
+
 ;;; Provide
 
 (provide 'utility)
