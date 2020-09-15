479c102d2d8329bbc86fbc5c26a04066da62b8f6
Yuan Fu <casouri@gmail.com>
2020-09-15 09:27:46 -0400

    Disable proselint because it checks iimg

--- star/checker.el
+++ star/checker.el
@@ -51,19 +51,19 @@
   :hook (text-mode . writegood-mode))
 
 ;; Install proselint by macports.
-(load-package flycheck
-  :hook (text-mode-hook . flycheck-mode)
-  :extern "proselint"
-  :config
-  (flycheck-define-checker proselint
-    "A linter for prose."
-    :command ("proselint" source-inplace)
-    :error-patterns
-    ((warning line-start (file-name) ":" line ":" column ": "
-	      (id (one-or-more (not (any " "))))
-	      (message) line-end))
-    :modes (text-mode org-mode))
-  (add-to-list 'flycheck-checkers 'proselint))
+;; (load-package flycheck
+;;   :hook (text-mode-hook . flycheck-mode)
+;;   :extern "proselint"
+;;   :config
+;;   (flycheck-define-checker proselint
+;;     "A linter for prose."
+;;     :command ("proselint" source-inplace)
+;;     :error-patterns
+;;     ((warning line-start (file-name) ":" line ":" column ": "
+;; 	      (id (one-or-more (not (any " "))))
+;; 	      (message) line-end))
+;;     :modes (text-mode org-mode))
+;;   (add-to-list 'flycheck-checkers 'proselint))
 
 (load-package langtool
   :config
