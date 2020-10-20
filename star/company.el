36bc55fb876a20a3cb4a029aaab346a41ee94e1b
Yuan Fu <casouri@gmail.com>
2020-10-19 22:59:36 -0400

    Update company config

--- star/company.el
+++ star/company.el
@@ -1,16 +1,5 @@
 ;; -*- lexical-binding: t -*-
 
-(defvar luna-company-manual nil
-  "If t, invoke company manually.")
-
-
-(when luna-company-manual
-  ;; we secretly swap tab and C-o so
-  ;; default bindings of tab (folding, indent, etc) goes to C-o
-  ;; (define-key input-decode-map (kbd "<tab>") (kbd "C-o"))
-  ;; (define-key input-decode-map (kbd "C-o") (kbd "<tab>"))
-  (global-set-key (kbd "<C-i>") #'company-complete)
-  (global-set-key (kbd "M-o") #'company-complete))
 (luna-def-key
  :keymaps '(company-active-map company-search-map)
  "C-p" #'company-select-previous
@@ -21,12 +10,12 @@
 (load-package company
   :hook (prog-mode-hook . company-mode)
   :config
-  (if luna-company-manual
-      (setq company-idle-delay nil
-            company-auto-complete t)
-    (setq company-idle-delay 0.1))
+  (setq company-idle-delay 0.1)
   (setq company-minimum-prefix-length 1
         company-dabbrev-downcase nil
         company-tooltip-limit 15)
+  ;; company dabbrev is annoying, make sure not to include it.
+  (setq-default company-backends
+                '(company-capf company-files company-dabbrev-code))
   (setq-default company-search-filtering t))
 
