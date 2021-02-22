8c96375ff85c5a6dc2a2fb027691c4ab894788fb
Yuan Fu <casouri@gmail.com>
2021-02-22 13:59:51 -0500

    Update tool bar icons

--- etc/images/bootstrap/convert.sh
+++ etc/images/bootstrap/convert.sh
@@ -1,6 +1,10 @@
 #!/opt/local/bin/fish
 
 for file in *.svg
-    cairosvg -o (basename $file .svg).png -s 3 $file
+    cairosvg -o (basename $file .svg).png -s 2 $file
+end
+
+for file in *.png
+    convert base.png $file -geometry +8+8 -composite $file
 end
 
