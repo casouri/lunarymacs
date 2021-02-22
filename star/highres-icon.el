39ba19b7523967bf36b6161882d71a437827c5a1
Yuan Fu <casouri@gmail.com>
2021-02-22 13:56:59 -0500

    Update icon config

--- star/highres-icon.el
+++ star/highres-icon.el
@@ -1,59 +1,6 @@
 ;; -*- lexical-binding: t; -*-
 
-;;; Image.el
-
-(setq image-resolution-scaling-factor 2)
-(push (expand-file-name "etc/images/" user-emacs-directory)
-      image-load-path)
-
-(defun find-image (specs)
-  "Find an image, choosing one of a list of image specifications.
-
-SPECS is a list of image specifications.
-
-Each image specification in SPECS is a property list.  The contents of
-a specification are image type dependent.  All specifications must at
-least contain the properties `:type TYPE' and either `:file FILE' or
-`:data DATA', where TYPE is a symbol specifying the image type,
-e.g. `xbm', FILE is the file to load the image from, and DATA is a
-string containing the actual image data.  The specification whose TYPE
-is supported, and FILE exists, is used to construct the image
-specification to be returned.  Return nil if no specification is
-satisfied.
-
-The image is looked for in `image-load-path'.
-
-Image files should not be larger than specified by `max-image-size'."
-  (let (image)
-    (while (and specs (null image))
-      (let* ((spec (car specs))
-	     (type (plist-get spec :type))
-	     (data (plist-get spec :data))
-	     (file (plist-get spec :file))
-             (high-res (when (> image-resolution-scaling-factor 1)
-                         (format "%s%s@%dx.%s"
-                                 (or (file-name-directory file) "")
-                                 (file-name-base file)
-                                 image-resolution-scaling-factor
-                                 (or (file-name-extension file) ""))))
-	     found)
-	(when (image-type-available-p type)
-	  (cond ((stringp file)
-                 (let ((high-res-found
-                        (and high-res (image-search-load-path high-res)))
-                       (found (image-search-load-path file)))
-                   (when (or high-res-found found)
-                     (let ((spec (copy-sequence spec)))
-                       (plist-put spec :file (or high-res-found found))
-                       (when high-res-found
-                         (plist-put
-                          spec :scale
-                          (/ 1.0 image-resolution-scaling-factor)))
-                       (setq image (cons 'image spec))))))
-		((not (null data))
-		 (setq image (cons 'image spec)))))
-	(setq specs (cdr specs))))
-    image))
+(require 'svg)
 
 ;;; Tab-line
 
@@ -64,52 +11,74 @@ Image files should not be larger than specified by `max-image-size'."
 
   (setq tab-line-new-button
         (propertize " + "
-                    'display `(image :type xpm
-                                     :file ,(image-search-load-path
-                                             "tabs/new@2x.xpm")
-                                     :margin (2 . 0)
-                                     :ascent center
-                                     :scale 0.5)
+                    'display
+                    (let ((svg (svg-create 9 9)))
+                      (svg-path svg '((moveto ((1 . 4.5) (8 . 4.5))))
+                                :stroke "black")
+                      (svg-path svg '((moveto ((4.5 . 1) (4.5 . 8))))
+                                :stroke "black")
+                      (svg-image svg :ascent 'center))
                     'keymap tab-line-add-map
                     'mouse-face 'tab-line-highlight
                     'help-echo "Click to add tab"))
 
   (setq tab-line-close-button
         (propertize " x"
-                    'display `(image :type xpm
-                                     :file ,(image-search-load-path
-                                             "tabs/close@2x.xpm")
-                                     :margin (2 . 0)
-                                     :ascent center
-                                     :scale 0.5)
+                    'display
+                    (let ((svg (svg-create 9 9)))
+                      (svg-path svg '((moveto ((1 . 1) (8 . 8))))
+                                :stroke "black"
+                                :stroke-width 1.5)
+                      (svg-path svg '((moveto ((1 . 8) (8 . 1))))
+                                :stroke "black"
+                                :stroke-width 1.5)
+                      (svg-image svg :ascent 'center))
                     'keymap tab-line-tab-close-map
                     'mouse-face 'tab-line-close-highlight
                     'help-echo "Click to close tab"))
 
   (setq tab-line-left-button
         (propertize " <"
-                    'display `(image :type xpm
-                                     :file ,(image-search-load-path
-                                             "tabs/left-arrow@2x.xpm")
-                                     :margin (2 . 0)
-                                     :ascent center
-                                     :scale 0.5)
+                    'display
+                    (let ((svg (svg-create 9 9)))
+                      (svg-path svg '((moveto ((5 . 0) (1 . 4.5)
+                                               (5 . 9))))
+                                :stroke "black"
+                                :stroke-width 1.5
+                                :fill "transparent")
+                      (svg-image svg :ascent 'center))
                     'keymap tab-line-left-map
                     'mouse-face 'tab-line-highlight
                     'help-echo "Click to scroll left"))
 
   (setq tab-line-right-button
         (propertize "> "
-                    'display `(image :type xpm
-                                     :file ,(image-search-load-path
-                                             "tabs/right-arrow@2x.xpm")
-                                     :margin (2 . 0)
-                                     :ascent center
-                                     :scale 0.5)
+                    'display
+                    (let ((svg (svg-create 9 9)))
+                      (svg-path svg '((moveto ((4 . 0) (8 . 4.5)
+                                               (4 . 9))))
+                                :stroke "black"
+                                :stroke-width 1.5
+                                :fill "transparent")
+                      (svg-image svg :ascent 'center))
                     'keymap tab-line-right-map
                     'mouse-face 'tab-line-highlight
                     'help-echo "Click to scroll right")))
 
 ;;; Widget
 
-(setq widget-image-directory "~/ed/etc/images/custom/")
+(defun highres-icon-widget-image-find (oldfn image)
+  (let ((fill (face-attribute 'default :foreground)))
+    (pcase image
+      ((or "down-pushed" "down")
+       (let ((svg (svg-create 9 9)))
+         (svg-polygon svg '((0 . 0) (9 . 0) (4.5 . 9)) :fill fill)
+         (svg-image svg)))
+      ((or "right-pushed" "right")
+       (let ((svg (svg-create 9 9)))
+         (svg-polygon svg '((0 . 0) (0 . 9) (8 . 4.5)) :fill fill)
+         (svg-image svg)))
+      (_ (funcall oldfn image)))))
+
+(advice-add #'widget-image-find :around #'highres-icon-widget-image-find)
+;; (advice-remove 'widget-image-find 'highres-icon-widget-image-find)
