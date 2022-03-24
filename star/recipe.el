d0e957b63c43e925b2b429d8cafc4f2e1b1cc008
Yuan Fu <casouri@gmail.com>
2022-03-23 18:33:51 -0700

    Add undo-hl

--- star/recipe.el
+++ star/recipe.el
@@ -13,4 +13,5 @@
         (valign . (:repo "casouri/valign" :option (:full-clone t)))
         (zeft . (:repo "casouri/zeft" :option (:full-clone t)))
         (xeft . (:repo "casouri/xeft" :option (:full-clone t)))
-        (vundo . (:repo "casouri/vundo" :option (:full-clone t)))))
+        (vundo . (:repo "casouri/vundo" :option (:full-clone t)))
+        (undo-hl . (:repo "casouri/undo-hl" :option (:full-clone t)))))
