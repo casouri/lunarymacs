424b97b76a56b3adba5439358dc9753c92d73e67
Yuan Fu <casouri@gmail.com>
2022-03-23 18:33:57 -0700

    Helpful workaround

--- star/app.el
+++ star/app.el
@@ -27,7 +27,25 @@
   (require 'erc-services)
   (erc-services-mode))
 
-(load-package helpful)
+(load-package helpful
+  :config
+  ;; FIXME: workaround before PR merges.
+  ;; https://github.com/Wilfred/helpful/issues/282
+  (defvar read-symbol-positions-list nil)
+  (defun helpful--autoloaded-p (sym buf)
+    "Return non-nil if function SYM is autoloaded."
+    (-when-let (file-name (buffer-file-name buf))
+      (setq file-name (s-chop-suffix ".gz" file-name))
+      (help-fns--autoloaded-p sym)))
+  (defun helpful--skip-advice (docstring)
+    "Remove mentions of advice from DOCSTRING."
+    (let* ((lines (s-lines docstring))
+           (relevant-lines
+            (--take-while
+             (not (or (s-starts-with-p ":around advice:" it)
+                      (s-starts-with-p "This function has :around advice:" it)))
+             lines)))
+      (s-trim (s-join "\n" relevant-lines)))))
 
 (load-package ghelp
   :commands
