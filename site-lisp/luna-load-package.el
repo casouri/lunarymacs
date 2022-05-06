fe9a6419be7c03a461c114daf15ba8e59dfd1916
Yuan Fu <casouri@gmail.com>
2022-05-06 15:06:46 -0700

    Add :download-name to load-package

--- site-lisp/luna-load-package.el
+++ site-lisp/luna-load-package.el
@@ -123,6 +123,9 @@ Available COMMAND:
   :extern        Add ARG to `luna-external-program-list'. ARG should
                  be a string \"PROGRAM\".  Use ‘luna-note-extern’ to
                  add note for PROGRAM.
+  :download-name If the name used to download the package is
+                 different from the package name, use this. The
+                 download name should be an unquoted symbol.
 
 Each COMMAND can take zero or more ARG. Among these commands,
 :autoload-hook, :commands, and :after expect literal arguments,
@@ -135,6 +138,10 @@ ARGS.
   (declare (indent 1))
   ;; Group commands and arguments together.
   (let* ((arg-list (luna-split-command-args args))
+         ;; The name used to download the package, PACKAGE is the
+         ;; package name, but sometimes it is different from the
+         ;; download name... (Eg, typescript-mode & tide).
+         (package-download-name package)
          ;; Translate commands & arguments to valid
          ;; config code.
          (body
@@ -144,6 +151,9 @@ ARGS.
                    (arg-list (cdr arg)))
                (pcase command
                  (:init arg-list)
+                 (:download-name
+                  (setq package-download-name (car arg-list))
+                  nil)
                  (:config `((with-eval-after-load ',package
                               ,@arg-list)))
                  ((or :hook :autoload-hook)
@@ -189,7 +199,7 @@ ARGS.
            ;; We need to add load-path before checking
            ;; if the package is installed or not.
            ,@load-path-form
-           (add-to-list 'luna-package-list ',package)
+           (add-to-list 'luna-package-list ',package-download-name)
            (when (not (luna-installed-p ',package))
              (error "%s not installed" ',package))
            ,@autoload-list
