8cdbda9202fe94ef9cd009220952ca13d7916b7d
Yuan Fu <casouri@gmail.com>
2023-05-07 16:11:48 -0700

    Make project.el aware of language-specific project files

--- star/edit.el
+++ star/edit.el
@@ -66,7 +66,25 @@
   :autoload-hook (prog-mode-hook . color-outline-mode))
 
 (with-eval-after-load 'project
-  (add-to-list 'project-vc-ignores ".ccls-cache/"))
+  (add-to-list 'project-vc-ignores ".ccls-cache/")
+  (add-to-list 'project-vc-ignores "node_modules")
+  (defvar project-language-aware-root-files
+    '("tsconfig.json"
+      "package.json"
+      "Cargo.toml"
+      "compile_commands.json"
+      "project.clj"
+      "compile_flags.txt"))
+  (defun project-try-language-aware (dir)
+    "Find a super-directory of DIR containing a root file."
+    (let ((dir (cl-loop for pattern in project-language-aware-root-files
+                        for result = (locate-dominating-file dir pattern)
+                        if result return result)))
+      (and dir (cons 'language-aware dir))))
+  (cl-defmethod project-root ((project (head language-aware)))
+    (cdr project))
+  (add-hook 'project-find-functions
+	        #'project-try-language-aware))
 
 (defun luna-project-find-regexp (regexp pattern)
   (interactive (list (project--read-regexp)
